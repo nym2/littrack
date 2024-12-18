efbc6fe4c2ca40a468b0a020605fc426065e733c
NymphaPamba <praisepamba@gmail.com>
2024-12-18 13:58:02 +0300

    initial commit

---
+++ initialize_db.py
@@ -0,0 +1,7 @@
+from database.db import create_tables
+
+if __name__ == "__main__":
+    print("Creating database and tables...")
+    create_tables()
+    print("Database and tables created successfully!")
+
